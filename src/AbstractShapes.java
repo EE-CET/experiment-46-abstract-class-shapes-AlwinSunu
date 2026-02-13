2b46b1f5881e1a9df686abc5e74cff7b265e83f2
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-13 19:50:01 +0530

    Update AbstractShapes.java

--- src/AbstractShapes.java
+++ src/AbstractShapes.java
@@ -1,43 +1,35 @@
 abstract class Shape {
-    // TODO: Abstract method numberOfSides()
-   abstract int numberOfSides();
+    abstract void numberOfSides();
 }
-class Rectangle extends Shape{
-int numberOfSides(){
-    System.out.println("4");
-    }
-}
-class Triangle extends Shape{
-int numberOfSides(){
-    System.out.println("3");
-    }
-}
-class Hexagon extends Shape{
-int numberOfSides(){
-    System.out.println("6");
-    }
-}
-// TODO: Create class Rectangle extends Shape
-// Implement numberOfSides() to print "Rectangle has 4 sides"
 
-// TODO: Create class Triangle extends Shape
-// Implement numberOfSides() to print "Triangle has 3 sides"
+class Rectangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Rectangle has 4 sides");
+    }
+}
 
-// TODO: Create class Hexagon extends Shape
-// Implement numberOfSides() to print "Hexagon has 6 sides"
+class Triangle extends Shape {
+    void numberOfSides() {
+        System.out.println("Triangle has 3 sides");
+    }
+}
+
+class Hexagon extends Shape {
+    void numberOfSides() {
+        System.out.println("Hexagon has 6 sides");
+    }
+}
 
 public class AbstractShapes {
-    public void main(String args[]){
+    public static void main(String args[]) {
+       
+        Rectangle a = new Rectangle();
+        Triangle b = new Triangle();
+        Hexagon c = new Hexagon();
 
-        Rectangle a=new Ractangle;
-        Triangle b=new Triangle;
-        Hexagon c=new Hexagon;
-
+      
         a.numberOfSides();
         b.numberOfSides();
         c.numberOfSides();
     }
-        // TODO: Create objects for Rectangle, Triangle, and Hexagon
-        // TODO: Call numberOfSides() for each object in the order: Rectangle, Triangle, Hexagon
-    
 }
